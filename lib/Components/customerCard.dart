bf92f2d9609d9a520fe0d46682aa58ec1295db2b
Zakaria <zakaria.aaddi@gmail.com>
2023-06-08 11:17:33 +0200

    customerCard Component created

---
+++ lib/Components/customerCard.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors, avoid_print
+
+import 'package:flutter/material.dart';
+
+class OutlinedCardExample extends StatelessWidget {
+  const OutlinedCardExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: const SizedBox(
+          width: 300,
+          height: 100,
+          child: Center(child: Text('Outlined Card')),
+        ),
+      ),
+    );
+  }
+}
