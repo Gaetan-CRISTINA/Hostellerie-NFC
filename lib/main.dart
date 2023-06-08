6a6fff1303f10d844c67e06c5ec10251c36cf71e
ArthurRaynal <arthur.raynal@le-campus-numerique.fr>
2023-06-08 09:45:16 +0200

    fixed overflow + moved login to Views folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, camel_case_types
-
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:hostellerie/Components/secondaryButton.dart';
-
-import 'Components/primaryButton.dart';
+import 'Views/login.dart';
 
 void main() => runApp(const MyApp());
 
@@ -36,83 +33,8 @@ class Home extends StatelessWidget {
         ),
         backgroundColor: Color(0xFFe6b34b),
       ),
-      body: const FirstPage(),
+      body: const Login(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final PageController controller = PageController();
-    return ScrollConfiguration(
-      behavior: ScrollConfiguration.of(context).copyWith(
-        dragDevices: {
-          PointerDeviceKind.touch,
-          PointerDeviceKind.mouse,
-        },
-      ),
-      child: Container(
-        color: Colors.white,
-        child: PageView(
-          controller: controller,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  child: Container(
-                    width: 400,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/logo.png"),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(top: 20, left: 50.0, right: 50.0),
-                  child: Divider(
-                    color: Color(0xFFe6b34b),
-                    height: 8,
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      labelText: 'Enter Name',
-                      hintText: 'Enter Your Name'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: primaryButton(
-                    textButton: 'Login',
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Text(
-                    'Forgot your password ?',
-                    style: TextStyle(
-                      color: Color(0xFF6200EF),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: secondaryButton(
-                    textButton: 'Cancel',
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
